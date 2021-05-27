e694df9f6c17959b4d518565b94672ac80c1f506
leonaminacio <leonamalves46@gmail.com>
2021-05-27 02:25:24 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+let l = 10
+while(1 <= l){
+    console.log(l)
+    l-=1
+}
+
+for (let y = 1; y <= 100; y+=2){
+    console.log(y)
+}
+
+let x = 0
+while(100 >= x){
+    console.log(x)
+    x+=2
+}
